9f208b64f9453349b1d0f6e12ca2af9d75b4b8ca
“Gimena” <gimenapascuale@gmail.com>
2023-07-24 13:21:23 -0300

    efectos y animaciones

--- js/style.js
+++ js/style.js
@@ -1,3 +1,11 @@
 function agregarAlCarrito() {
     alert("El producto ha sido agregado al carrito.");
   }
+
+  // Importar AOS
+import AOS from 'aos';
+// Importar los estilos CSS de AOS
+import 'aos/dist/aos.css';
+
+// Inicializar AOS
+AOS.init();
